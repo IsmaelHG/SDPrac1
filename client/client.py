75d5db5ba25e5b4ec6aac6224840af413d1dda37
IsmaelHG <40826314+IsmaelHG@users.noreply.github.com>
2021-03-10 11:19:25 +0100

    Primera version

--- client/client.py
+++ client/client.py
@@ -1,2 +1,10 @@
+import xmlrpc.client
+
 if __name__ == '__main__':
-    print("cliente")
+    proxy = xmlrpc.client.ServerProxy('http://localhost:9000')
+    print(proxy.add_worker())
+    print(proxy.add_worker())
+    print(proxy.add_worker())
+    print(proxy.add_worker())
+    print(proxy.list_workers())
+
--- cluster/master.py
+++ cluster/master.py
@@ -1,2 +1,65 @@
+from xmlrpc.server import SimpleXMLRPCServer
+import logging
+from multiprocessing import Process
+
+WORKERS = {}
+WORKER_ID = 0
+
+
+def submit_countingwords(files):
+
+    pass
+
+
+def submit_wordcount(files):
+    pass
+
+
+def add_worker():
+    global WORKERS
+    global WORKER_ID
+
+    proc = Process(target=start_worker, args=(WORKER_ID,))
+    proc.start()
+    WORKERS[WORKER_ID] = proc
+
+    WORKER_ID += 1
+
+    return WORKER_ID
+
+
+def delete_worker(id_worker):
+    global WORKERS
+    global WORKER_ID
+
+    proc = WORKERS[id_worker]
+    proc.kill()
+
+    WORKER_ID -= 1
+
+
+def list_workers():
+    return str(WORKERS)
+
+
+def start_worker():
+    pass
+
+
 if __name__ == '__main__':
-    print("cluster")
+    logging.basicConfig(level=logging.INFO)
+
+    server = SimpleXMLRPCServer(('localhost', 9000))
+
+    server.register_function(submit_countingwords, "submit_countingwords")
+    server.register_function(submit_wordcount, "submit_wordcount")
+
+    server.register_function(list_workers, "list_workers")
+    server.register_function(add_worker, "add_worker")
+    server.register_function(delete_worker, "delete_worker")
+
+    try:
+        print('Use Control-C to exit')
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print('Exiting')
