c4caa261f3ad7b8539f8ddc048fcc9d60048d27e
IsmaelHG <40826314+IsmaelHG@users.noreply.github.com>
2021-03-17 11:14:34 +0100

    Implementacion cola redis, start worker, countingwords

--- client/client.py
+++ client/client.py
@@ -2,9 +2,6 @@ import xmlrpc.client
 
 if __name__ == '__main__':
     proxy = xmlrpc.client.ServerProxy('http://localhost:9000')
-    print(proxy.add_worker())
-    print(proxy.add_worker())
-    print(proxy.add_worker())
-    print(proxy.add_worker())
-    print(proxy.list_workers())
+    print(proxy.submit_countingwords("[http://localhost:8000/fitxer1, http://localhost:8000/fitxer2]"))
+
 
--- cluster/master.py
+++ cluster/master.py
@@ -1,18 +1,48 @@
+import json
 from xmlrpc.server import SimpleXMLRPCServer
 import logging
 from multiprocessing import Process
+import redis
+import requests
 
 WORKERS = {}
 WORKER_ID = 0
+JOB_ID = 0
+global CONN
 
+COUNTWORDS = "CountingWords"
+WORDCOUNT = "WordCount"
+
+def countingwords():
+    pass
+
+
+def submit_task(files, type):
+    global JOB_ID
+    global CONN
+
+    filestr = files[1:-1]
+    filestr = filestr.split(",")
+
+    for file in filestr:
+        task = {
+            'JOBID': JOB_ID,
+            'TypeTask': type,
+            'fileurl': file
+        }
+        JOB_ID = JOB_ID + 1
+        CONN.rpush('task:queue', json.dumps(task))
+
+    if len(filestr) > 1:
+        #Llamar a join
+        pass
 
 def submit_countingwords(files):
-
-    pass
+    submit_task(files, COUNTWORDS)
 
 
 def submit_wordcount(files):
-    pass
+    submit_task(files, WORDCOUNT)
 
 
 def add_worker():
@@ -43,10 +73,32 @@ def list_workers():
 
 
 def start_worker():
-    pass
+    global CONN
+    while True:
+        task = CONN.blpop(['task:queue'], 0)
+        task_json = json.loads(task[1])
+        filestr = requests.get(task_json["fileurl"])
+        type = task_json["TypeTask"]
+
+        if type == WORDCOUNT:
+            number = WordCount(filestr)
+        elif type == COUNTWORDS:
+            number = CountingWords(filestr)
+
+        CONN.rpush(task_json["JOBID"], number)
+
+
+
+def CountingWords(str):
+    return len(str.split())
+
+def WordCount(str):
+    return 0
 
 
 if __name__ == '__main__':
+    CONN = redis.Redis()
+
     logging.basicConfig(level=logging.INFO)
 
     server = SimpleXMLRPCServer(('localhost', 9000))
